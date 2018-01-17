6966cdc7e3c7bb3f7dde0b5529e663ba27866751
no7ren <no7ren@gmail.com>
2018-01-17 15:21:38 +0100

    Update run_snp_filtering.sh

--- snp_filtering/run_snp_filtering.sh
+++ snp_filtering/run_snp_filtering.sh
@@ -8,6 +8,6 @@ sudo docker build -t bcftools:1.6 .
 sudo docker pull biocontainers/vcftools
 sudo docker pull lethalfang/tabix:1.2.1
 cd $1
-sudo docker run -it --user fangyj:fangyj -v `pwd`:`pwd` -w `pwd` --rm lethalfang/tabix:1.2.1 bgzip $2
-sudo docker run -it --user fangyj:fnagyj -v `pwd`:`pwd` -w `pwd` --rm bcftools:1.6 index $2.gz
-sudo docker run -it --user fangyj:fnagyj -v `pwd`:`pwd` -w `pwd` --rm bcftools:1.6 bcftools view -q $3:minor $2.gz | bcftools filter -i 'AD[1-] > $4' $2.gz | sudo docker run -it --user fangyj:fnagyj -v `pwd`:`pwd` -w `pwd` --rm vcftools vcftools --vcf - --max-missing $5 --remove-indels --out $2_filtered.vcf
+sudo docker run -it --user $(id -u) -v `pwd`:`pwd` -w `pwd` --rm lethalfang/tabix:1.2.1 bgzip $2
+sudo docker run -it --user $(id -u) -v `pwd`:`pwd` -w `pwd` --rm bcftools:1.6 index $2.gz
+sudo docker run -it --user $(id -u) -v `pwd`:`pwd` -w `pwd` --rm bcftools:1.6 bcftools view -q $3:minor $2.gz | bcftools filter -i 'AD[1-] > $4' $2.gz | sudo docker run -it --user $(id -u) -v `pwd`:`pwd` -w `pwd` --rm vcftools vcftools --vcf - --max-missing $5 --remove-indels --out $2_filtered.vcf
