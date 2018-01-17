4450477657b8eb36bd7034bdd7fbe451c1acda62
no7ren <no7ren@gmail.com>
2018-01-17 16:53:03 +0100

    Update run_snp_filtering.sh

--- snp_filtering/run_snp_filtering.sh
+++ snp_filtering/run_snp_filtering.sh
@@ -4,9 +4,10 @@
 #####third parameter is the number of minor allele frequency like 0.01, 0.05
 #####fourth parameter is the number of allelic depth to filter with. eg. 10 means at least one of the alternate alleles having an AD > 10
 #####fifth parameter is the maximum allowed percentage of missing data 
-sudo docker build -t bcftools:1.6 .
-sudo docker pull biocontainers/vcftools
-sudo docker pull lethalfang/tabix:1.2.1
+#####The following image has already been built so it's not necessary to run it again
+#sudo docker build -t bcftools:1.6 .
+#sudo docker pull biocontainers/vcftools
+#sudo docker pull lethalfang/tabix:1.2.1
 cd $1
 sudo docker run -it --user $(id -u) -v `pwd`:`pwd` -w `pwd` --rm lethalfang/tabix:1.2.1 bgzip $2
 sudo docker run -it --user $(id -u) -v `pwd`:`pwd` -w `pwd` --rm lethalfang/tabix:1.2.1 tabix $2
