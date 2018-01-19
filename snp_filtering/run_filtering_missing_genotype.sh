3e68a9652edfa72f507d1e3766e9aa9c7c6bbde3
no7ren <no7ren@gmail.com>
2018-01-19 11:59:13 +0100

    Update run_filtering_missing_genotype.sh

--- snp_filtering/run_filtering_missing_genotype.sh
+++ snp_filtering/run_filtering_missing_genotype.sh
@@ -3,10 +3,10 @@
 ####filtering missing genotype percentage on SNP data only: 10%, 20%, 30%, 40%, 50%
 ####the paremeter is the path of the indexed vcf file
 
-vcftools --gzvcf $1 --max-missing 0.1 --remove-indels --out perc1_missing
-vcftools --gzvcf $1 --max-missing 0.2 --remove-indels --out perc1_missing
-vcftools --gzvcf $1 --max-missing 0.3 --remove-indels --out perc1_missing
-vcftools --gzvcf $1 --max-missing 0.4 --remove-indels --out perc1_missing
-vcftools --gzvcf $1 --max-missing 0.5 --remove-indels --out perc1_missing
+vcftools --gzvcf $1 --max-missing 0.1 --remove-indels --out perc1_missing &
+vcftools --gzvcf $1 --max-missing 0.2 --remove-indels --out perc1_missing &
+vcftools --gzvcf $1 --max-missing 0.3 --remove-indels --out perc1_missing &
+vcftools --gzvcf $1 --max-missing 0.4 --remove-indels --out perc1_missing &
+vcftools --gzvcf $1 --max-missing 0.5 --remove-indels --out perc1_missing &
 
 
