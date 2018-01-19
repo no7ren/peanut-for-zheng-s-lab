823911b36117e5d396874d21fe2f0f92b9fe1435
no7ren <no7ren@gmail.com>
2018-01-19 11:57:59 +0100

    Update run_filtering_maf.sh

--- snp_filtering/run_filtering_maf.sh
+++ snp_filtering/run_filtering_maf.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 ###this is the script to run the filtering for maf 0.01, 0.02, 0.05, 0.10, 0.15, 0.20
+###the paremeter is the indexed vcf file
 
-vcftools --gzvcf $1 --maf 0.01 --out perc1 &
-vcftools --gzvcf $1 --maf 0.02 --out perc2 &
-vcftools --gzvcf $1 --maf 0.05 --out perc5 &
-vcftools --gzvcf $1 --maf 0.10 --out perc10 &
-vcftools --gzvcf $1 --maf 0.15 --out perc15 &
-vcftools --gzvcf $1 --maf 0.20 --out perc20 &
+vcftools --gzvcf $1 --maf 0.01 --remove-indels --out perc1 &
+vcftools --gzvcf $1 --maf 0.02 --remove-indels --out perc2 &
+vcftools --gzvcf $1 --maf 0.05 --remove-indels --out perc5 &
+vcftools --gzvcf $1 --maf 0.10 --remove-indels --out perc10 &
+vcftools --gzvcf $1 --maf 0.15 --remove-indels --out perc15 &
+vcftools --gzvcf $1 --maf 0.20 --remove-indels --out perc20 &
