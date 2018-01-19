271c6feb9dd5b1ebfbf5a1f99ff99b59b937457a
no7ren <no7ren@gmail.com>
2018-01-19 10:30:48 +0100

    Update run.sh

--- gatk_run/alignment/run.sh
+++ gatk_run/alignment/run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+run_processing(){
+   on=${1/.sam/_sorted.bam}
+   mn=${1/.sam/.metrics}
+   rn=${1/.sam/_rm.bam}
+   /home/sunzq/prog/gatk/gatk --java-options -Djava.io.tmpdir=/share/30t-3/tmp SortSam -I=$1 -SO=coordinate -O=$on
+   /home/sunzq/prog/gatk/gatk --java-options -Djava.io.tmpdir=/share/30t-3/tmp MarkDuplicates -MAX_FILE_HANDLES 1000 -I=$on -M=$mn -O=$rn
+   
+}
 cd $1
 N=6
 for i in *_1.fq.gz
@@ -9,5 +17,5 @@ do
     pu=`echo $i | cut -d"_" -f4`
     ld=`echo $i | cut -d"_" -f3`
     ((i=i%N)); ((i++==0)) && wait
-    /home/sunzq/minimap2/minimap2 -R '@RG\tID:$sm\tLD:$ld\tPL:illumina\tPU:$pu\tSM:$sm' -t 20 -2 -K 2G -/share/10t/fangyj/bin/arahy.Tifrunner.gnm1.KYV3.main_genome.fna $i $mn &    
+    /home/sunzq/minimap2/minimap2 -R '@RG\tID:$sm\tLD:$ld\tPL:illumina\tPU:$pu\tSM:$sm' -t 20 -2 -K 2G /share/10t/fangyj/bin/arahy.Tifrunner.gnm1.KYV3.main_genome.fna $i $mn &    
 done
