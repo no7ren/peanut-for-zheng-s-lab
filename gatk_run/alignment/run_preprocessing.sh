0947ab7bdd66cef928bd395418adaf367588ad4a
no7ren <no7ren@gmail.com>
2018-01-20 19:06:13 +0100

    Update and rename run.sh to run_preprocessing.sh

--- gatk_run/alignment/run_preprocessing.sh
+++ gatk_run/alignment/run_preprocessing.sh
@@ -6,9 +6,19 @@ run_processing(){
    rn=${1/.sam/_rm.bam}
    /home/sunzq/prog/gatk/gatk --java-options -Djava.io.tmpdir=/share/30t-3/tmp SortSam -I=$1 -SO=coordinate -O=$on
    /home/sunzq/prog/gatk/gatk --java-options -Djava.io.tmpdir=/share/30t-3/tmp MarkDuplicates -MAX_FILE_HANDLES 1000 -I=$on -M=$mn -O=$rn
-   docker run -it --rm -v /share/30t-1/new_alignment/:/share/30t-1/new_alignment/ biocontainers/samtools samtools index /share/30t-1/new_alignment/N402_rmdup_sm.bam
-   /home/sunzq/prog/gatk/gatk HaplotypeCaller --input N402_rmdup_sm.bam --output N402_haplotype --reference /share/10t/fangyj/bin/arahy.Tifrunner.gnm1.KYV3.main_genome.fna --TMP_DIR /share/30t-1/tmp --emit-ref-confidence GVCF --sample-name N402 --sample-ploidy 4
+   mv $rn split_tmp 
 }
+
+run_haplotype(){
+   bamtools split -in $1 -refPrefix splitTemp -reference
+   sm=`echo $1 | cut -d"_" -f1`
+   for i in *splitTemp*.bam
+   do
+       bamtools index $i
+       /home/sunzq/prog/gatk/gatk HaplotypeCaller --input $i --output $sm"_haplotype.vcf" --reference /share/10t/fangyj/bin/arahy.Tifrunner.gnm1.KYV3.main_genome.fna --TMP_DIR /share/30t-1/tmp --emit-ref-confidence GVCF --sample-name $sm
+   done
+}
+
 cd $1
 N=6
 for i in *_1.fq.gz
@@ -20,3 +30,5 @@ do
     ((i=i%N)); ((i++==0)) && wait
     /home/sunzq/minimap2/minimap2 -R '@RG\tID:$sm\tLD:$ld\tPL:illumina\tPU:$pu\tSM:$sm' -t 20 -2 -K 2G /share/10t/fangyj/bin/arahy.Tifrunner.gnm1.KYV3.main_genome.fna $i $mn &    
 done
+
+
